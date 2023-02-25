104552ff0d3c6b7b55c12554b9dfb6ac0393fb20
George Enciu <george.enciu@gmail.com>
2023-02-25 22:57:02 +0100

    adds express

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits } from 'discord.js';
 
 import ready from './interactions/ready';
 import interactionCreate from './interactions/interaction-create';
+import express, { Express } from 'express';
 
 export default () => {
   console.log('Bot is starting...');
@@ -14,4 +15,8 @@ export default () => {
   interactionCreate(client);
 
   client.login(process.env.DISCORD_TOKEN);
+  const app: Express = express();
+  app.listen(process.env.PORT, () => {
+    console.log('Server is running...');
+  });
 };
